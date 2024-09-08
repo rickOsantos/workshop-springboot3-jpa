13f89dabd0ffed42ca5f09b4e259bae3b15edfd5
Ricardo Santos <ricardo.eletris@gmail.com>
2024-09-08 12:20:36 -0300

    OrderStatus enum

--- src/main/java/com/ricardo/course/entities/Order.java
+++ src/main/java/com/ricardo/course/entities/Order.java
@@ -3,6 +3,8 @@ package com.ricardo.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.ricardo.course.entities.enums.OrderStatus;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,7 +22,10 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private Instant moment;
+	
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -29,10 +34,11 @@ public class Order implements Serializable {
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -59,6 +65,17 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus) ;
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+		
+	}
 
 	@Override
 	public int hashCode() {
@@ -84,4 +101,6 @@ public class Order implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 }
